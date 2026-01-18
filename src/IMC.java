5aef45d4b3354d861868317ad9b14f19ef0a690a
Jefferson f. Teodoro <jeffersonfrancateodoro@gmail.com>
2026-01-17 22:16:36 -0300

    Atualizando Projeto com Style.css

--- src/IMC.java
+++ src/IMC.java
@@ -5,25 +5,35 @@ import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.geometry.*;
 
+
 public class IMC extends Application {
 
     @Override
     public void start(Stage palco) {
         // Etiquetas para os campos de entrada
         Label etiquetaPeso = new Label("Peso");
+        etiquetaPeso.getStyleClass().add("label-peso");
         Label etiquetaAltura = new Label("Altura");
+        etiquetaAltura.getStyleClass().add("label-altura");
+
 
         // Campos de texto para entrada de dados
         TextField campoPeso = new TextField();
         campoPeso.setPromptText("Peso em kg");
+        campoPeso.getStyleClass().add("campo-peso");
+
         TextField campoAltura = new TextField();
         campoAltura.setPromptText("Altura em metros");
+        campoAltura.getStyleClass().add("campo-altura");
 
         // Label para mostrar o resultado do IMC
         Label etiquetaResultado = new Label();
 
         // Botão para calcular p IMC
         Button botaoCalcular = new Button("Calcular IMC");
+        botaoCalcular.getStyleClass().add("botao-calcular");
+
+
         botaoCalcular.setOnAction(e -> {
             try {
                 double peso = Double.parseDouble(campoPeso.getText().replace(',', '.'));
@@ -31,6 +41,7 @@ public class IMC extends Application {
 
                 double imc = peso / (altura * altura);
                 etiquetaResultado.setText(String.format("Seu IMC é: %.2f", imc));
+                etiquetaResultado.getStyleClass().add("label-resultado");
             }catch (NumberFormatException ex) {
                 etiquetaResultado.setText("Por favor, insira valores válidos para peso e altura.");
             }
@@ -43,6 +54,7 @@ public class IMC extends Application {
 
         // Cena e palco
         Scene cena = new Scene(layout, 350,250);
+        cena.getStylesheets().add("file:///C:/ProjetoCalculadoraIMC/CalculadoraIMC/src/Style.css");
         palco.setTitle("Calculadora de IMC");
         palco.setScene(cena);
         palco.show();
